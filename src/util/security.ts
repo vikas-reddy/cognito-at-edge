5dad768a40873cf675f4cd27b3b93aa9e99e558d
Vikas Reddy <vikred@amazon.com>
2023-04-24 12:13:45 -0700

    reformat comment

--- src/util/security.ts
+++ src/util/security.ts
@@ -66,17 +66,17 @@ export function sign(
 
 export const urlSafe = {
   /*
-        Functions to translate base64-encoded strings, so they can be used:
-        - in URL's without needing additional encoding
-        - in OAuth2 PKCE verifier
-        - in cookies (to be on the safe side, as = + / are in fact valid characters in cookies)
+  Functions to translate base64-encoded strings, so they can be used:
+  - in URL's without needing additional encoding
+  - in OAuth2 PKCE verifier
+  - in cookies (to be on the safe side, as = + / are in fact valid characters in cookies)
 
-        stringify:
-            use this on a base64-encoded string to translate = + / into replacement characters
+  stringify:
+      use this on a base64-encoded string to translate = + / into replacement characters
 
-        parse:
-            use this on a string that was previously urlSafe.stringify'ed to return it to
-            its prior pure-base64 form. Note that trailing = are not added, but NodeJS does not care
+  parse:
+      use this on a string that was previously urlSafe.stringify'ed to return it to
+      its prior pure-base64 form. Note that trailing = are not added, but NodeJS does not care
     */
   stringify: (b64encodedString) =>
     b64encodedString.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_'),
